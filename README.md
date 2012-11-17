38ffb65025eb2ed3eae40013b711e1cc0091ce53
Marcio Alves <malves1982@gmail.com>
2012-11-17 12:13:24 -0300

    edited filesin the root app directory.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ sproutcore_start
 ================
 
 projeto inicial do sprout
+
+todos_one
+todos_two
+todos_three
--- apps/todos_three/statechart.js
+++ apps/todos_three/statechart.js
@@ -1,8 +1,17 @@
 TodosThree.statechart = SC.Statechart.create({
+	trace: YES,
+	rootState: SC.State.design({
+		initialSubstate: "READY",
 
-  initialState: 'readyState',
+		READY: SC.State.plugin('TodosThree.READY_IN'),
+
+		LOGGING_IN: SC.State.plugin('TodosThree.LOGGING_APP'),
+
+		SHOWING_APP: SC.State.plugin('TodosThree.SHOWING_APP'),
+
+		SHOWING_DESTROY_CONFIRMATION: SC.State.plugin('TodosThree.SHOWING_DESTROY_CONFIRMATION')
+	})
   
-  readyState: SC.State.plugin('TodosThree.ReadyState'),
   // someOtherState: SC.State.plugin('TodosThree.SomeOtherState')
 
 });
